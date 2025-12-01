6035b74e83b93230a1714e8595b3143a21435a64
josephwere <josephogwe8@gmail.com>
2025-12-01 14:50:19 +0300

    Update page.tsx

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,23 +1,21 @@
-'use client';
+'use client'
 
 import React from 'react';
 
 import QueueManager from '../../components/QueueManager';
-import ThemeBuilder from '../../components/ui/ThemeBuilder';
-import RealtimeCharts from '../../components/ui/RealtimeCharts';
-import ChatV2 from '../../components/ChatV2';
+import RecommendationsPanel from '../../components/RecommendationsPanel';
 
 import AgentsPanel from '../../components/AgentsPanel';
 import EnginesPanel from '../../components/EnginesPanel';
 import VectorsPanel from '../../components/VectorsPanel';
 import JobsPanel from '../../components/JobsPanel';
-import RecommendationsPanel from '../../components/RecommendationsPanel';
 
 export default function DashboardPage() {
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '2fr 1fr', gap: 16 }}>
-      <div>
-        <h1>NeuroEdge Master Dashboard</h1>
+    <div className="grid grid-cols-3 gap-4 p-6">
+      {/* LEFT SIDE */}
+      <div className="col-span-2 space-y-6">
+        <h1 className="text-3xl font-bold">NeuroEdge Master Dashboard</h1>
 
         <AgentsPanel />
         <EnginesPanel />
@@ -25,11 +23,11 @@ export default function DashboardPage() {
         <JobsPanel />
       </div>
 
-      <aside>
+      {/* RIGHT SIDE */}
+      <aside className="space-y-6">
         <RecommendationsPanel />
         <QueueManager />
       </aside>
     </div>
   );
 }
-
